f0450eafaa70c89f44e52f3cb736788a52197b31
Yoo013 <alfraka744@gmail.com>
2024-06-10 21:11:34 +0700

    update5

--- Backend/index.js
+++ Backend/index.js
@@ -11,20 +11,23 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 
-const allowedOrigins = ['https://form-login-bice.vercel.app'];
+const allowedOrigins = [
+  'https://form-login-49ah.vercel.app',
+  'https://form-login-bice.vercel.app'
+];
 
 const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
-    methods: 'GET,POST',
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    optionsSuccessStatus: 204
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  methods: 'GET,POST',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  optionsSuccessStatus: 204
 };
 
 app.use(cors(corsOptions));
@@ -34,11 +37,11 @@ app.options('*', cors(corsOptions));
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('Hello Yousub Here, Home page');
+  res.send('Hello Yousub Here, Home page');
 });
 
 app.get('/msg', (req, res) => {
-    res.send('Welcome to my Api');
+  res.send('Welcome to my Api');
 });
 
 app.use('/api/users', UserRoutes);
@@ -46,17 +49,17 @@ app.use('/api/auth', AuthRoutes);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-    console.error('Internal Server Error:', err.message);
-    res.status(500).json({ message: 'Internal Server Error', error: err.message });
+  console.error('Internal Server Error:', err.message);
+  res.status(500).json({ message: 'Internal Server Error', error: err.message });
 });
 
 // Start server
 app.listen(PORT, async () => {
-    try {
-        await ConnectDB();
-        console.log(`Server running on port ${PORT}`);
-    } catch (error) {
-        console.error('Database connection failed:', error);
-        process.exit(1); // Exit process with failure
-    }
+  try {
+    await ConnectDB();
+    console.log(`Server running on port ${PORT}`);
+  } catch (error) {
+    console.error('Database connection failed:', error);
+    process.exit(1); // Exit process with failure
+  }
 });
